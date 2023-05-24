15b70443f05c523bb99c3a5c58471fb846778cc8
guyyariv <guyyariv111@gmail.com>
2023-05-24 09:51:35 +0300

    AudioTokenDemo

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ if __name__ == "__main__":
         outputs="image",
         title='AudioToken',
         description=description,
-        # examples=examples
+        examples=examples
     )
     demo.launch()
 
